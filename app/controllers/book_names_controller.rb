5c8d21b2bcf820a764dfb158a15077606fc1933b
Chloe Bradley <ccbradley77@gmail.com>
2015-02-08 19:07:04 -0700

    Finished book_names Crud

---
+++ app/controllers/book_names_controller.rb
@@ -0,0 +1,54 @@
+class BookNamesController < ApplicationController
+  def index
+    @book_names = BookName.all
+  end
+
+  def new
+    @book_name = BookName.new
+  end
+
+  def create
+    @book_name = BookName.new(book_name_params)
+    if @book_name.save
+      redirect_to book_names_path
+    else
+      @book_name = BookName.new(book_name_params)
+      render:new
+    end
+  end
+
+
+  def show
+    @book_name = BookName.find(params[:id])
+  end
+
+  def edit
+    @book_name = BookName.find(params[:id])
+  end
+
+  def update
+    @book_names = BookName.find(params[:id])
+    if @book_name.update(book_name_params)
+      redirect_to book_names_path
+    else
+      render :edit
+    end
+  end
+
+    def title
+      @book_name = BookName.title
+    end
+
+    def destroy
+      @book_name = BookName.find(params[:id])
+      @book_name.destroy
+      redirect_to book_names_path
+    end
+
+
+  private
+
+  def book_name_params
+    params.require(:book_name).permit(:title, :content, :author)
+  end
+end
